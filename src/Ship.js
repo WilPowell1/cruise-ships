ab3b62e9322e2b7395762b822b5b2355c896d122
WilPowell1 <powellwil88@gmail.com>
2020-05-30 21:37:16 +0100

    first commit

---
+++ src/Ship.js
@@ -0,0 +1,17 @@
+class Ship {
+    constructor(currentPort) {
+        this.currentPort = currentPort;
+        this.previousPort = null;
+    }
+
+    setSail() {
+        this.previousPort = this.currentPort;
+        this.currentPort = null;
+    }
+
+    dock(port) {
+        this.currentPort = port;
+    }
+};
+
+module.exports = Ship;
---
+++ test/Ship.test.js
@@ -0,0 +1,40 @@
+const Ship = require('../src/Ship.js');
+const Port = require('../src/Port.js');
+const Itinerary = require('../src/Itinerary.js');
+
+describe('Ship', () => {
+    it('can be instantiated', () => {
+
+        expect(new Ship()).toBeInstanceOf(Object);
+    });
+
+    it('has a starting port', () => {
+        const port = new Port('Dover');
+        const itinerary = new Itinerary([port]);
+        const ship = new Ship(itinerary);
+
+        expect(ship.currentPort).toBe(port);
+    });
+
+    it('can set sail', () => {
+        const port = new Port('Dover');
+        const itinerary = new Itinerary([port]);
+        const ship = new Ship(itinerary);
+
+        ship.setSail();
+
+        expect(ship.currentPort).toBeFalsy();
+    });
+
+    it('can dock at a different port', () => {
+        const dover = new Port('Dover');
+        const calais = new Port('Calais');
+        const itinerary = new Itinerary([dover, calais])
+        const ship = new Ship(itinerary);
+
+        ship.setSail();
+        ship.dock();
+
+        expect(ship.currentPort).toBe(calais);
+    })
+});
